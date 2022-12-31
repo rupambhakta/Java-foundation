e67f9ce96310f391b0f197c9a4ee5ee6639a1089
Rupam <rupambhakta2020@gmail.com>
2022-12-31 18:33:00 -0500

    problem added

--- Lecture_17/Codes/problem_3.java
+++ Lecture_17/Codes/problem_3.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class problem_4 {
+public class problem_3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
