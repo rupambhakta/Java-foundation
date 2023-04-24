2e731b54c2003e7972d157b4d79d81027ca3c415
Rupam <rupambhakta2020@gmail.com>
2023-04-24 21:06:02 +0530

    Assignment question 2

---
+++ Lecture_50 (OOPs)/Codes/assignmentQuestion_2.java
@@ -0,0 +1,23 @@
+//Q2. Create a class named as student having attributes like String name, String rollNumber and String 
+//    mobileNumber with a constructor accepting all the three attributes
+
+public class assignmentQuestion_2 {
+    public class student {
+        String name;
+        String rollNumber;
+        String mobileNumber;
+        public student(){
+
+        }
+
+        public student(String name, String rollNumber, String mobileNumber) {
+            this.name = name;
+            this.rollNumber = rollNumber;
+            this.mobileNumber = mobileNumber;
+        }
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
