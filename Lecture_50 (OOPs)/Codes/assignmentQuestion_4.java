9a55d15c9659db4b850427708bfff8cfcd546ae4
Rupam <rupambhakta2020@gmail.com>
2023-04-25 21:58:09 +0530

    Assignment question 4

---
+++ Lecture_50 (OOPs)/Codes/assignmentQuestion_4.java
@@ -0,0 +1,19 @@
+//Q4. Create a class named Animal having String name, String species and String genus along with an all 
+// argument constructor outside the main class. 
+public class assignmentQuestion_4 {
+    public static class animal {
+        String name;
+        String species;
+        String genus;
+
+        animal(String name, String species, String genus) {
+            this.name = name;
+            this.species = species;
+            this.genus = genus;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
