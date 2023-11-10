0650c3546557d7b4668275671c33967c1e2b179f
Rupam <rupambhakta2020@gmail.com>
2023-11-10 22:57:19 +0530

    reverse a queue

---
+++ Lecture_61  (Queue Interview Question)/reverse_a_queue.java
@@ -0,0 +1,26 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class reverse_a_queue {
+    public static void main(String[] args) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+        System.out.println(q);
+
+        Stack<Integer> st = new Stack<>();
+        while (q.size() != 0) {
+            st.push(q.remove());
+        }
+        
+        while (st.size() != 0) {
+            q.add(st.pop());
+        }
+
+        System.out.println(q);
+
+    }
+}
