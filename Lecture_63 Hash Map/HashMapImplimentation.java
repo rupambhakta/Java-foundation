9da0cc27ea010d2a292fc653c7b8e2cd04eb1dca
Rupam Bhakta <109302173+rupambhakta@users.noreply.github.com>
2024-08-10 20:11:41 +0530

    Incomplete

--- Lecture_63 Hash Map/HashMapImplimentation.java
+++ Lecture_63 Hash Map/HashMapImplimentation.java
@@ -1,5 +1,31 @@
 public class HashMapImplimentation {
+    static class MyHashMap<k, v> {
+        public static final int DEFAULT_CAPACITY = 4;
+        public static final float DEFAULT_LOAD_FACTOR = 0.75f;
+        private class Node{
+            k key;
+            v value;
+            Node( k key, v value){
+                this.key = key;
+                this.value =value;
+            }
+        }
+
+
+        public int size(){
+            //Retuen the number of entries in map
+            return 0;
+        }
+
+        public void put(k key, v value){
+            
+        }
+    }
+
     public static void main(String[] args) {
+        MyHashMap<String,Integer> mp = new MyHashMap<>();
+        //Wirte a code to put data on mp 
+
         
     }
 }
