87299e9bde4e49a530ba31104c0515640683d86d
Rupam Bhakta <109302173+rupambhakta@users.noreply.github.com>
2024-08-19 02:09:42 +0530

    Done

--- Lecture_66 Backtracking/printPath.java
+++ Lecture_66 Backtracking/printPath.java
@@ -2,7 +2,7 @@ public class printPath {
     public static void main(String[] args) {
         int rows = 3;
         int cols = 3;
-        PrintMaze(1, 1, rows, cols, "");
+        PrintMaze(0, 0, rows-1, cols-1, "");
     }
 
     private static void PrintMaze(int sr, int sc, int er, int ec, String s) {
