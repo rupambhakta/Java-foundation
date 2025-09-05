b78db242a6c2f2a3d40b49dc16c1d2ed2ec89805
Rupam Bhakta <rupambhakta2020@gmail.com>
2025-09-06 00:32:21 +0530

    Second largest element in a array

---
+++ IntervireQuestion/SecondLargestElementInArray.java
@@ -0,0 +1,15 @@
+public class SecondLargestElementInArray {
+    public static void main(String[] args) {
+        int arr[] = { 12, 35, 1, 10, 35, 1 };
+        int first = Integer.MIN_VALUE, second = Integer.MIN_VALUE;
+        for (int n : arr) {
+            if (n > first) {
+                second = first;
+                first = n;
+            } else if (n > second && n != first) {
+                second = n;
+            }
+        }
+        System.out.println("Secodn Largest Element this this array is : "+second);
+    }
+}
