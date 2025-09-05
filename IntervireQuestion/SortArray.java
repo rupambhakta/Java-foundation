7e2ec94c55ce16c5d0f371a2cdeab4744af0b8e2
Rupam Bhakta <rupambhakta2020@gmail.com>
2025-09-06 00:32:29 +0530

    Sort a array

---
+++ IntervireQuestion/SortArray.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class SortArray {
+    public static void main(String[] args) {
+        int arr[] = {1,2,2,3,4,4,4,5};
+        Arrays.sort(arr);;
+        System.out.println(Arrays.toString(arr));
+    }
+}
