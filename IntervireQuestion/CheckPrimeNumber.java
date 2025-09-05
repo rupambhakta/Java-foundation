97f7d92e6fa9e9715565a707a92ab23f5f38a414
Rupam Bhakta <rupambhakta2020@gmail.com>
2025-09-05 21:36:35 +0530

    Prime number

---
+++ IntervireQuestion/CheckPrimeNumber.java
@@ -0,0 +1,15 @@
+public class CheckPrimeNumber {
+    static Boolean CheckPrime(int n) {
+        if (n <= 1)
+            return false;
+        for (int i = 2; i < Math.sqrt(n); i++) {
+            if (n % i == 0)
+                return false;
+        }
+        return true;
+    }
+
+    public static void main(String args[]) {
+        System.out.println(CheckPrime(17));
+    }
+}
